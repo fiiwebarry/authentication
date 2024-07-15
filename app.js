ca03e13c99e8f84bb8af3c6eea6313ee8a6b4df3
precious <ikpaprecious@gmail.com>
2024-07-15 07:39:07 +0100

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const authRoute = require('./routes/authenticationRoute');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const authRoute = require("./routes/authenticationRoute");
 // const testRoute = require('./routes/testRoute');
 const app = express();
 
@@ -11,13 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
-    console.log('connected sucessfully');
+    console.log("Database connected sucessfully");
   })
   .catch((error) => {
-    console.log('an error occured', error);
+    console.log("an error occured", error);
   });
 
-app.use('/auth', authRoute);
+app.use("/auth", authRoute);
 // app.use('/test', testRoute);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- utils/emailUtil.js
+++ utils/emailUtil.js
@@ -1,19 +1,20 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const tp = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
-  secure: true,
-  port: 465,
 });
+
 async function sendEmail(to, subject, body) {
   await tp.sendMail({
-    from: 'fiiwebarry@gmail.com',
-    to: '',
+    from: "ikpaprecious2@gmail.com",
+    to,
     subject,
     html: body,
   });
